b2eabbf736a2de22f3e2c7b723f5e6764adc690d
Dev_Dennis <98164069+OrangeLiberty@users.noreply.github.com>
2022-12-04 14:53:56 +0100

    cron time adjust

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const CronJob = require("cron").CronJob;
 
 var bot = require("./src/bot");
 
-const job = new CronJob("0 8,12,16,20 * * 1-7", () => {
+const job = new CronJob("0 8,12,16,20 * * 0-6", () => {
     bot.postRandomQuote();
 });
 
