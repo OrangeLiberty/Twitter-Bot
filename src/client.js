0dabd8783628a729069edb0a690f1e1174e29157
Dev_Dennis <98164069+OrangeLiberty@users.noreply.github.com>
2022-12-13 20:15:17 +0100

    set up server

--- src/client.js
+++ src/client.js
@@ -8,7 +8,5 @@ module.exports = {
     consumer_secret: process.env.APPSECRET,
     access_token: process.env.ACCESSTOKEN,
     access_token_secret: process.env.ACCESSSECRET,
-    timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
-    strictSSL: true, // optional - requires SSL certificates to be valid.
     character_limit: 240,
 };
